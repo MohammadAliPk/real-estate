1c558d830dc79d79a1fe7725c4487f59b94cef47
Mohammad Ali <aliparkour83@gmail.com>
2024-01-08 17:57:06 +0330

    header changes

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { FiLogIn } from "react-icons/fi";
+import styles from "@/layout/Header.module.css";
+import Link from "next/link";
+
+const Header: FC = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">صفحه اصلی</Link>
+          </li>
+          <li>
+            <Link href="/buy-residential">آگهی ها</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.login}>
+        <Link href="/signin">
+          <FiLogIn />
+          <span>ورود</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
