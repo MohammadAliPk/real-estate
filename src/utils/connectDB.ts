c82ba11e607efb1ff0b2762c81d197e80ce607dd
Mohammad Ali <aliparkour83@gmail.com>
2024-01-09 01:21:38 +0330

    ConnectDB function has been created

---
+++ src/utils/connectDB.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+type MongoUri = string | undefined;
+
+async function connectDB() {
+  const mongoUri: MongoUri = process.env.MONGO_URI || "";
+  if (mongoose.connections[0].readyState) return;
+  mongoose.set("strict", true);
+  await mongoose.connect(mongoUri);
+  console.log("Connected to Mongo");
+}
